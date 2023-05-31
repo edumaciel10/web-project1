ee528fe7d6913c0d1ec9f66fac56f85ad4078e98
Eduardo Maciel de Matos <eduardo.maciel@usp.br>
2023-05-31 20:08:50 -0300

    fix(vuetify): fix vuetify components import and add users route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import UsersView from '../views/UsersView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/users',
+      name: 'users',
+      component: UsersView
+    }
   ],
 });
 
