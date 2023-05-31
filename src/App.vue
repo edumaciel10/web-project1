b3b47e395d6b29a8d37b2a59b66bf76fbf1410e3
Joao Otavio <joao0otavio3@gmail.com>
2023-05-31 17:05:17 -0300

    feat(footer): add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script lang="ts">
+import FooterComponent from './components/FooterComponent.vue';
+
 export default {
   name: 'App',
+  components: { FooterComponent },
 };
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="rgb(10, 44, 87)"> ICMC </v-app-bar>
+    <v-app-bar color="var(--blue)"> ICMC </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
+
+    <footer-component />
   </v-app>
 </template>
+
+<style>
+  :root {
+    --blue: rgb(10, 44, 87);
+  }
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default {
+  name: 'FooterComponent',
+};
+</script>
+
+<template>
+  <v-footer color="var(--blue)" height="1" class="FooterComponent">
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <p class="text-center text-white">SSC0961 - Desenvolvimento Web e Mobile - 2023.1</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<style scoped>
+  .FooterComponent {
+    max-height: 100px;
+  }
+</style>
