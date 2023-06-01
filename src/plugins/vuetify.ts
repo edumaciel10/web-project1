46cc49df3406f4c1ccde8533e9a66f5957e01a75
Joao Otavio <joao0otavio3@gmail.com>
2023-05-31 22:11:35 -0300

    fix style and vuetify components import

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,14 @@
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+
+import * as components from 'vuetify/components';
+import * as labsComponents from 'vuetify/labs/components';
 import * as directives from 'vuetify/directives';
 
 import '@mdi/font/css/materialdesignicons.css';
-import  * as components from 'vuetify/labs/components';
 
 const vuetify = createVuetify({
-  components,
+  components: { ...components, ...labsComponents },
   directives,
 });
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import UsersView from '../views/UsersView.vue';
+import DefesasView from '../views/DefesasView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +11,10 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/users',
-      name: 'users',
-      component: UsersView
-    }
+      path: '/defesas',
+      name: 'defesas',
+      component: DefesasView,
+    },
   ],
 });
 
