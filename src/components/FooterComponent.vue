784b4968452a11ba1e1dc4ba1eeaa8224f5ba836
Joao Otavio <joao0otavio3@gmail.com>
2023-06-02 15:35:55 -0300

    feat(header): add logo and nav tabs

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -18,6 +18,6 @@ export default {
 
 <style scoped>
 .FooterComponent {
-  max-height: 100px;
+  max-height: 56px;
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  name: 'HeaderComponent',
+};
+</script>
+
+<template>
+  <v-app-bar color="var(--blue)">
+    <v-row>
+      <v-col class="d-flex align-center">
+        <v-img src="/logo.png" alt="logo icmc" width="107" height="48" align="center" />
+      </v-col>
+
+      <v-col cols="7">
+        <v-tabs centered color="white">
+          <v-tab to="/" class="text-white"> Home </v-tab>
+          <v-tab to="/defesas" class="text-white"> Defesas </v-tab>
+        </v-tabs>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
