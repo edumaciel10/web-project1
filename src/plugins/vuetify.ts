d0862e0371c55f3c0565a560e2d85c3d61e771cb
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2023-06-03 23:37:34 -0300

    fix(dependency): remove unused dependency (#12)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,11 +6,10 @@ import * as labsComponents from 'vuetify/labs/components';
 import * as directives from 'vuetify/directives';
 
 import '@mdi/font/css/materialdesignicons.css';
-import { Skeleton } from 'vue-loading-skeleton';
 
 // In jsx template
 const vuetify = createVuetify({
-  components: { ...components, ...labsComponents, Skeleton },
+  components: { ...components, ...labsComponents },
   directives,
 });
 
