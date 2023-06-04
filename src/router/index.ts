4d53aaabe1488b6d42aa72ab380b6d6bfbf10ca1
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2023-06-03 23:02:51 -0300

    feat(defesa): add visualization of defesa (#11)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import DefesasView from '../views/DefesasView.vue';
+import HomeView from '@/views/HomeView.vue';
+import DefesasView from '@/views/DefesasView.vue';
+import DefesaView from '@/views/DefesaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/defesas',
       name: 'defesas',
       component: DefesasView,
+    },
+    {
+      path: '/defesas/:id',
+      name: 'defesa',
+      component: DefesaView,
     },
   ],
 });
---
+++ src/utils/cursosType.ts
@@ -0,0 +1,7 @@
+export type DefesasType = {
+  Curso: string;
+  Programa: string;
+  Ordem: string;
+  Nome: string;
+  Data: string;
+};
